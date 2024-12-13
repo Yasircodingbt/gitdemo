88d6c44475d0f6133aee651495e167aba0761f8b
yasircodingbt <yasircoding_bt@outlook.com>
2024-12-13 19:22:32 +0500

    reverse of any number with recursion

---
+++ Recursion/recursion-1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+void repeated(int n);
+int main(){
+     int a ;
+     printf("enter a num :");
+     scanf("%d",&a);
+     repeated(a);
+     return 0;
+}void repeated(int n){
+     if (n == 0){
+       return ;              /* base code */
+     }
+     printf("%d\n",n);
+     repeated(n-1);
+     return;
+}
