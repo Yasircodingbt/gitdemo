246b1c41c95003a17be10bd401d42c5f185b2a48
yasircodingbt <yasircoding_bt@outlook.com>
2024-12-13 19:25:01 +0500

    function of recursion with the formale of factorial

---
+++ Recursion/recursion-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int factorial(int n);
+int main()
+{
+     int n;
+     printf("enter a num :");
+     scanf("%d", &n);
+     int fact = factorial(n);
+     printf("the factorial of %d is %d", n, fact);
+     return 0;
+}
+int factorial(int x){
+     if (x == 1 || x == 0){  // base case
+        return 1;
+     }
+     return x * (x - 1);
+}
