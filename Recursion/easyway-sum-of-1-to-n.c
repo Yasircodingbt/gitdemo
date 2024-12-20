60f8b0c590d4077b9c7002fdd585911a4deeb10e
yasircodingbt <yasircoding_bt@outlook.com>
2024-12-20 16:45:58 +0500

    An easyway to printf sum of n to 1 number with recursion

---
+++ Recursion/easyway-sum-of-1-to-n.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int sum(int n); // function
+int main()
+{
+     int n;
+     printf("enter a num :");
+     scanf("%d", &n);
+     int s = sum(n);      // function calling
+     printf("the sum of %d is %d", n, s);
+     return 0;
+}
+int sum(int x)
+{
+     if (x == 1 || x == 0)  // base code
+     { 
+          return 1; 
+     }
+      int n = x + sum(x - 1);  //  using formula of sum x+(x-1)
+  return n;
+}
