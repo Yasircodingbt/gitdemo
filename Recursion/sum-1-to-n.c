f49b4bd49f3cb9872098e47774e00e808ef8d205
yasircodingbt <yasircoding_bt@outlook.com>
2024-12-20 17:08:39 +0500

    An other method to solve the sum of n to 1 number with recursion

--- Recursion/sum-1-to-n.c
+++ Recursion/sum-1-to-n.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 #include<math.h>
-void sum(int a ,int b);
+void sum(int a ,int b);  // function
 int main(){
      int a,b;
 
      printf("enter a num : ");
      scanf("%d", &a);
-  sum(a,0);
+  sum(a,0);    /*calling function*/
 
      return 0;
 }void sum(int a,int b){
   if (a == 0){
-     printf("%d",b);
+     printf("%d",b);  // base code
      return;
   }
-     sum(a-1,b+a);
+     sum(a-1,b+a); //  paremater
 
      return;
 }
