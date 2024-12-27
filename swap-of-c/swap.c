efcd20ce44e964aadb17134f1412d7352657bf0b
yasircodingbt <yasircoding_bt@outlook.com>
2024-12-27 20:30:49 +0500

    swap to value with third variable

--- swap-of-c/swap.c
+++ swap-of-c/swap.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 #include<math.h>
-
 int main(){
      int a,b,temp;
      printf("enter a num : ");
      scanf("%d",&a);
      printf("enter a sec num : ");
      scanf("%d",&b);
-
-temp = a;
-a =b;
-b = temp;
-printf("enter a swap num  (a) is %d",a);
+       temp = a;
+      a =  b;
+    b = temp;
+printf("entring a swap num  (a) is %d",a);
      return 0;
-}
+     }
