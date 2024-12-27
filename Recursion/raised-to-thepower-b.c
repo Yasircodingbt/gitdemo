5a041697e548c1874262ebe5432bcaaf1cd0d4da
yasircodingbt <yasircoding_bt@outlook.com>
2024-12-27 20:25:16 +0500

    a raised to power of b

--- Recursion/raised-to-thepower-b.c
+++ Recursion/raised-to-thepower-b.c
@@ -3,7 +3,7 @@ raised to the power 'b' using recursion*/
 #include<stdio.h>
 #include<time.h>
 #include<math.h>
-int power(int a, int b);
+    int power(int x, int y);
 int main(){
 int a,b;
 printf("enter a number : ");
@@ -11,14 +11,17 @@ scanf("%d", &a);
 printf("enter a number : ");
 scanf("%d", &b);
  int p = power(a,b);
- printf("%d",p);
+ printf("%d \n",p);
+ /* simple method of power function*/
+int sample = pow(2,8);
+  printf("answer power is %d",sample);
 
 return 0;
 
-}int power(int a, int b){
-      if(b == 0 ){
+}int power(int x, int y){
+      if(y == 0 ){
           return 1;
       }
- return a*power(a,b-1);
+ return x*power(x,y-1);
 
 }
