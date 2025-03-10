798664909df1316208d93c865400093ee4618cc6
Yasircondingbt <185803128+Yasircodingbt@users.noreply.github.com>
2025-03-10 19:23:23 +0500

    call by  refrences

--- swap-of-c/swap2.c
+++ swap-of-c/swap2.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <math.h>
-void swap(int a,int b);
+void swap(int *a,int *b){
+   
+  *a = *a + *b;
+  *b = *a - *b;
+  *a = *a - *b;
+    return;
+}
 int main()
 {
      int a, b;
@@ -8,10 +14,7 @@ int main()
      scanf("%d", &a);
      printf("enter a sec num : ");
      scanf("%d", &b);
-       printf(" %d");
-       a = a + b;
-       b = a - b;
-       a = a - b;
+    swap(&a,&b);
    printf("%d",a);
    
        return 0;
